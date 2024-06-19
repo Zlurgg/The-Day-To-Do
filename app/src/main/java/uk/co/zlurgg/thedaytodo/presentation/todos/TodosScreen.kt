88c530d8b9e7c9a5abfaf5ddd1747b0b20140aa4
Zlurgg <joseph.brightman@gmail.com>
2024-06-19 14:55:20 +0100

    order section impl

--- app/src/main/java/uk/co/zlurgg/thedaytodo/presentation/todos/TodosScreen.kt
+++ app/src/main/java/uk/co/zlurgg/thedaytodo/presentation/todos/TodosScreen.kt
@@ -1,23 +1,40 @@
 package uk.co.zlurgg.thedaytodo.presentation.todos
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.List
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import uk.co.zlurgg.thedaytodo.presentation.todos.components.OrderSection
 import uk.co.zlurgg.thedaytodo.presentation.todos.components.TodoItem
 
 @Composable
@@ -38,24 +55,69 @@ fun TodosScreen(
             }
         },
     ) { innerPadding ->
-        LazyColumn(
+        Column(
             modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
                 .padding(innerPadding)
-                .fillMaxSize(),
-            contentPadding = PaddingValues(16.dp)
         ) {
-            items(state.todos) { todo ->
-                TodoItem(
-                    todo = todo,
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Todo List:",
+                    style = MaterialTheme.typography.headlineMedium
+                )
+                IconButton(
+
+                    onClick = {
+                        viewModel.onEvent(TodosEvent.ToggleOrderSection)
+                    },
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.List,
+                        contentDescription = "Sort"
+                    )
+                }
+            }
+            AnimatedVisibility(
+                visible = state.isOrderSectionVisible,
+                enter = fadeIn() + slideInVertically(),
+                exit = fadeOut() + slideOutVertically()
+            ) {
+                OrderSection(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .clickable {
-                            navController.navigate("add_edit_todo_screen" + "?todoId=${todo.id}")
-                        },
-                    onDeleteClick = {
-                        viewModel.onEvent(TodosEvent.DeleteTodo(todo))
+                        .padding(vertical = 16.dp),
+                    todoOrder = state.todoOrder,
+                    onOrderChange = {
+                        viewModel.onEvent(TodosEvent.Order(it))
                     }
                 )
+            }
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                items(state.todos) { todo ->
+                    TodoItem(
+                        todo = todo,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                navController.navigate("add_edit_todo_screen" + "?todoId=${todo.id}")
+                            },
+                        onDeleteClick = {
+                            viewModel.onEvent(TodosEvent.DeleteTodo(todo))
+                        }
+                    )
+                }
             }
         }
     }
